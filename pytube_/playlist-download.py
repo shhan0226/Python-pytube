44bfb2408c443893ca7fb89129c11beb2fc59dc5
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2022-03-22 23:33:02 +0900

    Update playlist-download.py

--- pytube_/playlist-download.py
+++ pytube_/playlist-download.py
@@ -1,8 +1,7 @@
 import os
 from pytube import YouTube
 from pytube import Playlist
-
-#p = Playlist('https://www.youtube.com/playlist?list=PLRrUisvYoUw9-cTYgkbTbr9f9CpbGdq4F')
+#p = Playlist('https://www.youtube.com/playlist?...')
 url_list = input("url: ?")
 p = Playlist(url_list)
 print(f'Downloading: {p.title}')
@@ -11,13 +10,18 @@ print(file_name)
 
 # playlist download
 for video in p.video_urls:
-	yt = YouTube(video)
-	print(yt.title)
-#	for i, stream in enumerate(yt.streams.all()):
-#		print(i, " : ", stream)
-	for i, stream in enumerate(yt.streams.filter(progressive=True, file_extension="mp4", type="video").all()):
-		print(i, " : ", stream)
-	yt.streams.get_by_itag(22).download('/data/Youtube/haru/' + file_name)
+    try:    
+        yt = YouTube(video)
+        print(yt.title)
+#        for i, stream in enumerate(yt.streams.all()):
+#    	    print(i, " : ", stream)
+        for i, stream in enumerate(yt.streams.filter(progressive=True, file_extension="mp4", type="video").all()):
+            print(i, " : ", stream)
+        yt.streams.get_by_itag(22).download('/data/Youtube/haru/' + file_name)
+    except:
+        print("download Error...                           ")
+        print(video)
+        print("                                            ")
 
 # youtube download
 #yt = YouTube('url address')

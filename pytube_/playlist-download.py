9b5b9cee3e4e158f716ba637a15a98b60f505422
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2022-03-22 22:02:53 +0900

    Update playlist-download.py

--- pytube_/playlist-download.py
+++ pytube_/playlist-download.py
@@ -5,7 +5,10 @@ from pytube import Playlist
 p = Playlist('https://www.youtube.com/playlist?list=PLQNlK4RYJxm8u3_aZ5ZyYsvfjnHLVpfRN')
 print(f'Downloading: {p.title}')
 
+
+# playlist download
 for video in p.video_urls:
+	print(video)
 	yt = YouTube(video)
 	print(yt.title)
 #	for i, stream in enumerate(yt.streams.all()):
@@ -13,3 +16,7 @@ for video in p.video_urls:
 	for i, stream in enumerate(yt.streams.filter(progressive=True, file_extension="mp4", type="video").all()):
 		print(i, " : ", stream)
 	yt.streams.get_by_itag(22).download('/data')
+
+# youtube download
+#yt = YouTube('url address')
+#yt.streams.get_by_itag(22).download('/data')

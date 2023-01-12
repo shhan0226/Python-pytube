f1a74a4002207a1efb2ee497c635732857a2258c
shhan <shhan0226@gmail.com>
2023-01-12 19:03:35 +0900

    update 2023.01.12

--- googletrans/FileToTran.py
+++ googletrans/FileToTran.py
@@ -36,11 +36,13 @@ if __name__ == '__main__':
         trans = translator.translate(target, src="en", dest="ko")
         print("\n+---------------")
         log_str.append("\n+--------------")
-        print(trans.text)
-        log_str.append(trans.text)
-        trans_target = ">>(" + target+ ")"
+
+        trans_target = ">>(" + target+ ")"        
         print(trans_target)
         log_str.append(trans_target)
+        
+        print(trans.text)
+        log_str.append(trans.text)
 
     log_str = '\n'.join(log_str)
     _log_write_(log_str)
